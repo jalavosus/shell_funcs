fe8c7b75c78715fe146aaa89bae1aa2d7857222a
James Alavosus <alavosus.james@gmail.com>
2021-03-21 21:32:37 -0400

    Update usage template a bit

--- templates/usage.sh
+++ templates/usage.sh
@@ -1,22 +1,22 @@
 usage () {
   return_code=$1
 
-  cat <<HEREDOC
-  [COMMAND NAME HERE]
+cat <<HEREDOC
+[COMMAND NAME HERE]
 
-  Description:
-    Command description here
+Description:
+  Command description here
 
-  Usage:
-    COMMAND_NAME [-h] [-d|--debug] [-v|--value <val>] <positional>
+Usage:
+  COMMAND_NAME [-h] [-d|--debug] [-v|--value <val>] <positional>
 
-  Parameters:
-    <positional> : [parameter] description
+Parameters:
+  <positional> : description of positional argument
 
-  Options:
-    -v|--value <val>  : [optn] value of this option [default: fork]
-    -d|--debug        : [flag] a boolean flag maybe [default: false]
-    -h|--help 				: [flag] show this usage text
+Options:
+  -v|--value <val>  : value of this option [default: fork]
+  -d|--debug        : a boolean flag maybe [default: false]
+  -h|--help 				: show this usage text
 HEREDOC
 
   return $return_code
